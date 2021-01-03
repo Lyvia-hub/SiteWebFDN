32468afe43193e6c65ed1b6f55037ea33e3d72f8
Lyvia Blay <lyviablay@gmail.com>
2021-01-03 14:27:15 -0500

    Creation and design of the banner (home page)

--- src/app/public/home/home.module.ts
+++ src/app/public/home/home.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { MainPageComponent } from './main-page/main-page.component';
+import { MainBannerComponent } from './main-banner/main-banner.component';
 
 
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, MainBannerComponent],
   imports: [
     SharedModule
   ]
