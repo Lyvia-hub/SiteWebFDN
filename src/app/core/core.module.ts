96448a13acb3afb762e60ccbf43d204d14d471a4
Lyvia Blay <lyviablay@gmail.com>
2021-01-03 11:46:12 -0500

    Creation and design of the footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,16 +2,23 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublicModule } from '../public/public.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [
+    NavbarComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule,
     PublicModule
   ],
-  exports: [NavbarComponent]
+  exports: [
+    NavbarComponent,
+    FooterComponent
+  ]
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
