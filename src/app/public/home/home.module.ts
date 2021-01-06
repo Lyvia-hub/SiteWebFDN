f73dc3cb96221bdd1cffcb92743b76823f4224a0
Lyvia Blay <lyviablay@gmail.com>
2021-01-05 23:44:15 -0500

    Creation and design of the Team section

--- src/app/public/home/home.module.ts
+++ src/app/public/home/home.module.ts
@@ -4,11 +4,18 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { MainBannerComponent } from './main-banner/main-banner.component';
 import { MainFdnPresentationComponent } from './main-fdn-presentation/main-fdn-presentation.component';
 import { MainSvodComponent } from './main-svod/main-svod.component';
+import { MainTeamComponent } from './main-team/main-team.component';
 
 
 
 @NgModule({
-  declarations: [MainPageComponent, MainBannerComponent, MainFdnPresentationComponent, MainSvodComponent],
+  declarations: [
+    MainPageComponent,
+    MainBannerComponent,
+    MainFdnPresentationComponent,
+    MainSvodComponent,
+    MainTeamComponent
+  ],
   imports: [
     SharedModule
   ]
